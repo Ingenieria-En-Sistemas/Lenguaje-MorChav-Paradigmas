447ed49cb4ed1bb5f4f911ceb32f0f89ba0fa139
FloresAnders <126523827+FloresAnders@users.noreply.github.com>
2023-10-21 22:49:51 -0600

    Update Lexer.py

---
+++ Lexer.py
@@ -0,0 +1,48 @@
+import re
+
+# Lista de tokens
+tokens = [
+    ('NUMBER', r'\d+'),
+    ('PLUS', r'\+'),
+    ('MINUS', r'-'),
+    ('TIMES', r'\*'),
+    ('DIVIDE', r'/'),
+    ('LPAREN', r'\('),
+    ('RPAREN', r'\)'),
+]
+
+# Clase Token para almacenar información sobre cada token
+class Token:
+    def __init__(self, token_type, value):
+        self.type = token_type
+        self.value = value
+
+# Función lexer que divide la entrada en tokens
+def lexer(input_string):
+    token_list = []
+    input_string = input_string.replace('\n', '')  # Elimina saltos de línea
+    pos = 0
+
+    while pos < len(input_string):
+        match = None
+        for token_type, pattern in tokens:
+            regex = re.compile(pattern)
+            match = regex.match(input_string, pos)
+            if match:
+                value = match.group(0)
+                token_list.append(Token(token_type, value))
+                pos = match.end()
+                break
+
+        if not match:
+            print(f"Token no reconocido: {input_string[pos]}")
+            return None
+
+    return token_list
+
+# Prueba del lexer
+data = "3+4*10-20"
+token_list = lexer(data)
+if token_list:
+    for token in token_list:
+        print(f"Token: {token.type}, Valor: {token.value}")

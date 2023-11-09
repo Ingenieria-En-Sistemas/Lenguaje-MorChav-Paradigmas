f8ad3260b6ca1421287357de17532f70e6322641
FloresAnders <126523827+FloresAnders@users.noreply.github.com>
2023-11-09 14:47:24 -0600

    update IDE

--- IDE.py
+++ IDE.py
@@ -2,11 +2,12 @@ import customtkinter
 from Lexer import lexer
 from Parser import parse_program
 from Interpreter import evaluate
-from tkinter import PhotoImage
-from PIL import Image, ImageTk
+from customtkinter import CTkImage  # Importar CTkImage desde customtkinter
+from PIL import Image
+import random
 
-customtkinter.set_appearance_mode("System") # Modos: system (predeterminado), light, dark
-customtkinter.set_default_color_theme("blue") # Temas: blue (predeterminado), dark-blue, green
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("dark-blue")
 
 def execute_expression():
     input_expr = input_text.get("1.0", "end-1c")
@@ -47,42 +48,57 @@ def clear_input():
     output_text.delete("1.0", "end")
     output_text.configure(state="disabled")
 
-# Configurar la ventana principal
-app = customtkinter.CTk() # crear ventana CTk como se hace con la ventana Tk
-app.title("MorChav IDE")
+# Lista de títulos aleatorios
+random_titles = [
+    "El odio es bueno si nos hace seguir adelante. (Sandor ‘El Perro’ Clegane)",
+    "Cuando se juega al Juego de Tronos, solo se puede ganar o morir. (Cersei Lannister)",
+    "Los dioses no tienen piedad, por eso son dioses. (Cersei Lannister)",
+    "Las serpientes enfadadas atacan. Eso hace más fácil aplastar sus cabezas. (Daenerys Targaryen)",
+    "Cualquier hombre que deba decir ‘soy el rey’, no es un verdadero rey. (Tywin Lannister)",
+]
 
-# Ajustar el tamaño de la ventana
+# Función para cambiar el título de la ventana
+def change_window_title():
+    new_title = random.choice(random_titles)
+    app.title(new_title)
+
+# Configurar la ventana principal
+app = customtkinter.CTk()
 app.geometry("1000x625")
 
 # Crear un contenedor para los botones y centrarlos
 button_container = customtkinter.CTkFrame(app)
 button_container.pack()
 
-# Cargar los iconos como imágenes
-run_icon = Image.open("run.png")  # Reemplaza con la ubicación de tu icono "Ejecutar"
-clear_icon = Image.open("clear.png")  # Reemplaza con la ubicación de tu icono "Limpiar"
+# Cargar las imágenes como CTkImage y ajustar el tamaño
+run_image = Image.open("run.png")  # Reemplaza con la ubicación de tu icono "Ejecutar"
+run_icon = CTkImage(light_image=run_image, size=(50, 50))  # Ajusta el tamaño a 32x32 píxeles
 
-# Convierte las imágenes a objetos PhotoImage
-run_icon = ImageTk.PhotoImage(run_icon)
-clear_icon = ImageTk.PhotoImage(clear_icon)
+clear_image = Image.open("clear.png")  # Reemplaza con la ubicación de tu icono "Limpiar"
+clear_icon = CTkImage(light_image=clear_image, size=(50, 50))  # Ajusta el tamaño a 32x32 píxeles
 
 # Crear un campo de entrada de texto desplazable más grande
 input_text = customtkinter.CTkTextbox(app, wrap=customtkinter.WORD, width=500, height=350)
-input_text.configure(font=("Consolas", 17)) 
-input_text.pack(padx=10, pady=(10, 5), fill="both")  # Aumenta la altura y ajusta el relleno vertical
+input_text.configure(font=("Consolas", 17))
+input_text.pack(padx=10, pady=(10, 5), fill="both")
 
 # Crea el botón para ejecutar la expresión con el icono
-execute_button = customtkinter.CTkButton(button_container, text="Ejecutar", image=run_icon, compound="left", command=execute_expression)
-execute_button.pack(side="left", padx=10)  # Alinea el botón "Ejecutar" a la izquierda
+execute_button = customtkinter.CTkButton(button_container, text="Run", image=run_icon, compound="left", command=execute_expression)
+execute_button.configure(font=("Consolas", 17))
+execute_button.pack(side="left", padx=10)
 
 # Crea el botón para limpiar el campo de entrada y el campo de salida con el icono
-clear_button = customtkinter.CTkButton(button_container, text="Limpiar", image=clear_icon, compound="left", command=clear_input)
-clear_button.pack(side="left", padx=10)  # Alinea el botón "Limpiar" a la izquierda
+clear_button = customtkinter.CTkButton(button_container, text="Clean", image=clear_icon, compound="left", command=clear_input)
+clear_button.configure(font=("Consolas", 17))
+clear_button.pack(side="left", padx=10)
 
 # Crear un campo de salida de texto desplazable más grande
 output_text = customtkinter.CTkTextbox(app, state="disabled", wrap=customtkinter.WORD, width=60, height=200)
-output_text.configure(font=("Consolas", 17)) 
-output_text.pack(padx=10, pady=(5, 10), fill="both")  # Aumenta la altura y ajusta el relleno vertical
+output_text.configure(font=("Consolas", 17))
+output_text.pack(padx=10, pady=(5, 10), fill="both")
+
+# Carga y cambia el título de la ventana al iniciar
+change_window_title()
 
 # Ejecutar la aplicación
 app.mainloop()

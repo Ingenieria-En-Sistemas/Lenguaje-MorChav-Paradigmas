282bbcbd11196f79452c75154f17b1f6fb4de455
ALCHACAS <alvaro.chaves.cascante@est.una.ac.cr>
2023-11-21 22:30:24 -0600

    UPDATEupdate

--- IDE.py
+++ IDE.py
@@ -48,16 +48,26 @@ def add_selected_option_to_input_text():
     if selected_option == "IF":
         input_text.insert(
             "end",
-            """NORTE(2!=2){\n\tdracarys("Verdadero")\n}SUR{\n\tdracarys("Falso")\n}ENDNORTE""",
+            """NORTE(2!=2){
+    dracarys("Verdadero")
+}SUR{
+    dracarys("Falso")
+}ENDNORTE""",
         )
     elif selected_option == "FOR":
-        input_text.insert("end", f"VIAJE(espada i = 1 to 10 step 2) dracarys(i*2)")
+        input_text.insert("end", f"""VIAJE(espada i = 1 to 10 step 2)
+    dracarys(i*2)""")
     elif selected_option == "PRINT":
         input_text.insert("end", f"""dracarys("Hola mundo")""")
     elif selected_option == "WHILE":
         input_text.insert(
             "end",
-            """\nespada i = 1 while(i<10){\n\tDRACARYS("Menor que 10")\n i=i+1\n}endwhile""",
+            """ 
+espada i = 1
+while(i<10){
+    DRACARYS("Menor que 10")
+    i=i+1
+}endwhile""",
         )
     elif selected_option == "BOOLEAN":
         input_text.insert(
